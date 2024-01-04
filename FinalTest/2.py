a65640e8c3a27f612027ee19026a8ff7ed4e69bc
a5y5y579 <110591292+a5y5y579@users.noreply.github.com>
2024-01-04 11:25:47 +0800

    0104

--- FinalTest/2.py
+++ FinalTest/2.py
@@ -33,4 +33,44 @@ print(cold_drink)
 print(hot_drink)
 print(cold_drink1)
 
+class Employee:
+    def __init__(self, name, experience, work_hours):
+        self.name = name
+        self.experience = experience
+        self.work_hours = work_hours
 
+    def get_name(self):
+        # 查詢名字
+        return self.name
+
+    def get_experience(self):
+        # 查詢年資
+        return self.experience
+
+    def get_work_hours(self):
+        # 查詢工時
+        return self.work_hours
+
+    def calculate_salary(self):
+        # 假設月薪計算為每小時工資乘以工作時數
+        hourly_wage = 186.0  # 假設每小時工資為186元
+        return hourly_wage * self.work_hours
+
+
+employee1 = Employee("蛋寶", 5, 160)
+employee2 = Employee("熱狗", 3, 140)
+employee3 = Employee("ozi", 1, 120)
+
+# 呼叫副函式
+print("員工姓名:", employee1.get_name())
+print("員工年資:", employee1.get_experience(), "年")
+print("員工小時數:", employee1.get_work_hours(), "小時")
+print("員工月薪:", employee1.calculate_salary(), "元")
+print("員工姓名:", employee2.get_name())
+print("員工年資:", employee2.get_experience(), "年")
+print("員工小時數:", employee2.get_work_hours(), "小時")
+print("員工月薪:", employee2.calculate_salary(), "元")
+print("員工姓名:", employee3.get_name())
+print("員工年資:", employee3.get_experience(), "年")
+print("員工小時數:", employee3.get_work_hours(), "小時")
+print("員工月薪:", employee3.calculate_salary(), "元")
